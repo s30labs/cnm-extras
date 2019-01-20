8742881c73ea09f58c95ad8567d5759e0b8307ac
Fernando Marin <fmarin@s30labs.com>
2019-01-20 19:09:00 +0100

    [IMP] Included module Filesys-SmbClient-3.2 (Filesys-SmbClient-3.2.sh needed)

---
+++ perl_modules/Filesys-SmbClient-3.2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#-----------------------------------------------------------------------
+echo ">>>>>>>EXEC $0"
+
+#-----------------------------------------------------------------------
+DIR_BASE=`pwd`
+BUILD_DIR=/tmp/src/perl
+mkdir -p $BUILD_DIR
+
+#-----------------------------------------------------------------------
+# Se hace por script por la vriable CPATH al hacer gcc
+tar -C $BUILD_DIR -xzvf /os/perl_modules/Filesys-SmbClient-3.2.tar.gz
+cd $BUILD_DIR/Filesys-SmbClient-3.2
+
+export CPATH=/usr/include/samba-4.0/ && perl Makefile.PL
+make
+make install
+
